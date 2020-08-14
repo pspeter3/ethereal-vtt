36e08061ff5aee733c59e96c647ba323fbd6f6bd
Phips Peter <pspeter333@gmail.com>
2020-08-14 11:05:53 -0700

    Add Webpack
    Add Webpack for production and local development.

---
+++ webpack.config.ts
@@ -0,0 +1,107 @@
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import OptimizeCSSAssetsPlugin from "optimize-css-assets-webpack-plugin";
+import path from "path";
+import TerserJSPlugin from "terser-webpack-plugin";
+import webpack from "webpack";
+
+class LangPlugin {
+    public static readonly Name = "LangPlugin";
+
+    private lang: string;
+
+    constructor(lang: string) {
+        this.lang = lang;
+    }
+
+    apply(compiler: webpack.Compiler) {
+        compiler.hooks.compilation.tap(LangPlugin.Name, (compilation) => {
+            HtmlWebpackPlugin.getHooks(compilation).beforeEmit.tap(
+                LangPlugin.Name,
+                (data) => {
+                    data.html = data.html.replace(
+                        "<html",
+                        `<html lang="${this.lang}"`
+                    );
+                    return data;
+                }
+            );
+        });
+    }
+}
+
+const configure = (
+    _: unknown,
+    { mode }: { mode: "none" | "development" | "production" }
+): webpack.Configuration => {
+    const isProd = mode === "production";
+    return {
+        entry: path.join(__dirname, "src", "index.ts"),
+        output: {
+            filename: isProd ? "[name].[chunkhash].js" : "[name].js",
+            publicPath: "/",
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    exclude: /node_modules/,
+                    use: [
+                        {
+                            loader: "ts-loader",
+                            options: {
+                                compilerOptions: {
+                                    module: "esnext",
+                                },
+                            },
+                        },
+                    ],
+                },
+                {
+                    test: /\.css?$/,
+                    use: [
+                        {
+                            loader: MiniCssExtractPlugin.loader,
+                        },
+                        { loader: "css-loader", options: { importLoaders: 1 } },
+                        {
+                            loader: "postcss-loader",
+                            options: {
+                                ident: "postcss",
+                                plugins: [],
+                            },
+                        },
+                    ],
+                },
+            ],
+        },
+        resolve: {
+            extensions: [".tsx", ".ts", ".js"],
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                title: "Ethereal VTT",
+                scriptLoading: "defer",
+                meta: {
+                    description: "Minimal peer to peer virtual table top",
+                },
+            }),
+            new MiniCssExtractPlugin({
+                filename: isProd ? "[name].[contenthash].css" : "[name].css",
+            }),
+            new LangPlugin("en"),
+        ],
+        optimization: {
+            minimizer: [
+                new TerserJSPlugin({}),
+                new OptimizeCSSAssetsPlugin({}),
+            ],
+            runtimeChunk: true,
+            splitChunks: {
+                chunks: "all",
+            },
+        },
+    };
+};
+
+export default configure;
