e9fb2780c0710f906a8a2ab4f2b826116a870dd0
Phips Peter <pspeter333@gmail.com>
2020-08-15 12:58:50 -0700

    Add design system packages
    Add tailwindcss-classnames for managing classes.
    Add Feather for icons.

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,10 @@ import { h, render } from "preact";
 
 export const NAME = "Ethereal VTT";
 
-window.addEventListener("load", () => {
-    render(<h1>{NAME}</h1>, document.body);
-}, {once: true});
+window.addEventListener(
+    "load",
+    () => {
+        render(<h1>{NAME}</h1>, document.body);
+    },
+    { once: true }
+);
