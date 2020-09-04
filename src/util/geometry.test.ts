2201a3fe77acc3a2fceaf1596a225bcd74976292
Phips Peter <pspeter3@users.noreply.github.com>
2020-09-04 11:48:16 -0700

    Improve geometry naming (#20)
    Change the naming of geometric functions and types.
    Also add Line and Path types with a measure helper.
    
    Thanks @wjohnson191 for the suggestions.

--- src/util/geometry.test.ts
+++ src/util/geometry.test.ts
@@ -1,4 +1,4 @@
-import { distance } from "./geometry";
+import { distance, euclidean, measure } from "./geometry";
 
 describe("geometry", () => {
     describe("distance", () => {
@@ -14,6 +14,32 @@ describe("geometry", () => {
 
         it("should handle diagonals", () => {
             expect(distance([0, 0], [4, 4])).toEqual(6);
+        });
+    });
+
+    describe("euclidian", () => {
+        it("should handle distance", () => {
+            expect(euclidean([0, 0], [3, 4])).toEqual(5);
+        });
+    });
+
+    describe("measure", () => {
+        it("should measure a path", () => {
+            expect(
+                measure([
+                    [0, 0],
+                    [6, 0],
+                    [10, 4],
+                ]),
+            ).toEqual(12);
+        });
+
+        it("should handle an empty array", () => {
+            expect(measure([])).toEqual(0);
+        });
+
+        it("should handle an array of a single position", () => {
+            expect(measure([[0, 0]])).toEqual(0);
         });
     });
 });
